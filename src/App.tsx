f03dec9e2874c4d5518e6f16c20269b14331f222
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-07-08 15:38:18 -0300

    Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from "./static/Footer/Footer";
+import Navbar from "./static/Navbar/Navbar";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from "./views/Home/Home";
+import Profile from "./views/Profile/Profile";
+export default function App() {
+	return (
+		<>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/Profile"  element={<Profile />} />
+			</Routes> 
+		</Router>
+		<Footer />
+		</>
+	)
 }
-
-export default App;
